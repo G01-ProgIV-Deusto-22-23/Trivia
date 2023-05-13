9cb32251b83b4a7e94f0d88b163dd43a9d1a85c0
Mikel Pintado <mikelpint@protonmail.com>
2023-05-13 23:59:05 +0200

    Chincheto albañil

--- src/local/main.c
+++ src/local/main.c
@@ -1,11 +1,5 @@
 trivia_main {
     setup_ui ();
 
-    size_t m = create_menu (choicemenu, 0, 0, 0, 0, ((const char *[]) { "jijiji" }));
-    timeout_menu (m, 10);
-    display_menu (m);
-
-    exit (0);
-
     welcome ();
 }
